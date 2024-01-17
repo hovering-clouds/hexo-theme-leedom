dd2556fd2e6e619d92e3368781d4c3be25621d17
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2024-01-17 20:52:07 +0800

    duration info

---
+++ source/js/duration.js
@@ -0,0 +1,31 @@
+!(function() {
+    /** 计时起始时间，自行修改 **/
+    var start = new Date("2024/01/17 12:00:00");
+  
+    function update() {
+      var now = new Date();
+      now.setTime(now.getTime()+250);
+      days = (now - start) / 1000 / 60 / 60 / 24;
+      dnum = Math.floor(days);
+      hours = (now - start) / 1000 / 60 / 60 - (24 * dnum);
+      hnum = Math.floor(hours);
+      if(String(hnum).length === 1 ){
+        hnum = "0" + hnum;
+      }
+      minutes = (now - start) / 1000 /60 - (24 * 60 * dnum) - (60 * hnum);
+      mnum = Math.floor(minutes);
+      if(String(mnum).length === 1 ){
+        mnum = "0" + mnum;
+      }
+      seconds = (now - start) / 1000 - (24 * 60 * 60 * dnum) - (60 * 60 * hnum) - (60 * mnum);
+      snum = Math.round(seconds);
+      if(String(snum).length === 1 ){
+        snum = "0" + snum;
+      }
+      document.getElementById("timeDate").innerHTML = "Online for &nbsp"+dnum+"&nbspdays";
+      document.getElementById("times").innerHTML = hnum + "&nbsphours&nbsp" + mnum + "&nbspminutes&nbsp" + snum + "&nbspseconds.";
+    }
+  
+    update();
+    setInterval(update, 1000);
+  })();
